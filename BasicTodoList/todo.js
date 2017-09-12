0d64cee6d38277fada0e4314add869f2349acec3
timfoo <timothyfoomj@gmail.com>
2017-09-12 23:46:17 +0800

    added delete function, refactored code

--- BasicTodoList/todo.js
+++ BasicTodoList/todo.js
@@ -1,4 +1,4 @@
-var todos = [];
+var todos = ["Buy cheese"];
 var input = prompt("What would you like to do?")
 if(input === "list"){
   console.log(todos);
@@ -12,21 +12,38 @@ todos.push(newTodo);
 while(input !== "quit"){
   // handle input
   if(input === "list"){
-    console.log(todos);
+    listTodos();
   }
   else if (input === "new") {
-    // ask for new todo
-    var newTodo = prompt("Enter new todo");
-    // add to todos array
-    todos.push(newTodo);
+    listNew();
   }
   else if (input === "delete"){
-    // check which to delete
-    var deleteTask = prompt("which one would you like to delete?")
-    // delete the task
-    todos.splice(todos.indexOf(deleteTask),1);
+    listDelete();
   }
   // ask for new input
   input = prompt("What would you like to do?")
 }
 console.log("Ok, you have quit the app")
+
+function listTodos(){
+  console.log("**********")
+  todos.forEach(function(todos,index){
+    console.log(index + ": "+ todos);
+  });
+  console.log("**********")
+}
+
+function listNew(){
+  // ask for new todo
+  var newTodo = prompt("Enter new todo");
+  // add to todos array
+  todos.push(newTodo);
+}
+
+function listDelete(){
+  // check which to delete
+  var deleteTask = prompt("which one would you like to delete? - Enter index")
+  // delete the task
+  todos.splice(deleteTask,1);
+  console.log("You've deleted the task!")
+}
