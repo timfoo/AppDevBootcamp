561bac258fff35e28173b8a61d83a8d0d9c9dac4
timfoo <30102954+timfoo@users.noreply.github.com>
2018-04-07 19:12:13 +0800

    reorganised

---
+++ Javascript/DOM/countingEventsExercise/countingEventsExercise.js
@@ -0,0 +1,6 @@
+// Count how many events are listed on the MDN Event reference page
+// https://developer.mozilla.org/en-US/docs/Web/Events
+
+var tr = document.querySelectorAll("tr");
+var table = document.querySelectorAll("table")
+tr.length - table.length
